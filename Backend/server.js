80b70cfaa3c50ec382f41b0c5f346c0016073286
Rudecapybere <affithusky@gmail.com>
2023-02-28 18:48:56 +0530

    nodemon

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,10 @@
 const express =require('express');
+const dotenv =require("dotenv");
 const { chats } =require("./data/data");
 
 
 const app = express();
+dotenv.config();
 
 app.get('/', (req, res) => {
     res.send("API is running");
@@ -17,9 +19,11 @@ app.get('/api/chat',(req,res) => {
 app.get('/api/chat/:id',(req,res)=>{
     // console.log(req.params.id);
 
-    const singlechat = chats.find((c)=>c.id === req.params.id);
+    const singlechat = chats.find((c)=>c._id === req.params.id);
     res.send(singlechat);
 
-})
+});
 
-app.listen(5000,console.log("server is started on port 5000"));
+const PORT= process.env.PORT || 5000
+
+app.listen(5000,console.log("server is started on port ${PORT}"));
