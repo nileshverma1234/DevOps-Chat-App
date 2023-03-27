e1eebcb5dc08010ef42ec29ca8602859c879849c
Shiv13527 <yuvrajpundir7788@gmail.com>
2023-03-27 22:37:10 +0530

    userRoutes and user registration API, installing bcryptjs

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -7,8 +7,7 @@ const userSchema = mongoose.Schema(
     email: { type: "String", unique: true, required: true },
     password: { type: "String", required: true },
     pic: {
-      type: "String",
-      required: true,
+      type: String,
       default:
         "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
