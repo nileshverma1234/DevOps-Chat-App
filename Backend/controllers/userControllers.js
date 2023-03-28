ad357226decc4494aefa681815d7cb0baedaa175
Shiv13527 <yuvrajpundir7788@gmail.com>
2023-03-28 19:37:31 +0530

    done generateToken.js

--- Backend/controllers/userControllers.js
+++ Backend/controllers/userControllers.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
-// const generateToken = require('../config/generateToken');
+const generateToken = require('../config/generateToken');
 const registerUser = asyncHandler(async (req,res) => {
     const { name, email, password, pic } = req.body;
 
