f0b0db3766e89757ccccb505c02c837e6f7cffdf
Nilesh <nv63354@gmail.com>
2023-04-08 15:35:52 +0530

    Chat API routes are defined and Chat API Created

--- Backend/routes/chatRoutes.js
+++ Backend/routes/chatRoutes.js
@@ -1,1 +1,16 @@
-   
+const express = require("express");
+const { protect } = require("../middleware/authMiddleware");
+
+const {
+  accessChat
+} = require("../controllers/chatControllers");
+const router = express.Router();
+
+router.route("/").post(protect, accessChat);
+// router.route("/").get(protect, fetchChats);
+// router.route("/group").post(protect, createGroupChat);
+// router.route("/rename").put(protect, renameGroup);
+// router.route("/groupremove").put(protect, removeFromGroup);
+// router.route("/groupadd").put(protect, addToGroup);
+
+module.exports = router;
