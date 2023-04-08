117d71058b4e3bf624128a0c0474f15761f64811
Nilesh <nv63354@gmail.com>
2023-04-08 15:49:51 +0530

    fetch user chat API

--- Backend/routes/chatRoutes.js
+++ Backend/routes/chatRoutes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
 
 const {
-  accessChat
+  accessChat,
+  fetchChats
 } = require("../controllers/chatControllers");
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
-// router.route("/").get(protect, fetchChats);
+router.route("/").get(protect, fetchChats);
 // router.route("/group").post(protect, createGroupChat);
 // router.route("/rename").put(protect, renameGroup);
 // router.route("/groupremove").put(protect, removeFromGroup);
