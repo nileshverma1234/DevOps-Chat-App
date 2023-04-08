8efc697a21254196501d8f9d7ba30f2daa429f24
Rudecapybere <affithusky@gmail.com>
2023-04-08 18:32:31 +0530

    Rename group api

--- Backend/controllers/chatControllers.js
+++ Backend/controllers/chatControllers.js
@@ -104,6 +104,28 @@ const createGroupChat = asyncHandler(async (req, res) => {
         throw new Error(error.message);
     }
 
+    const renameGroup = asyncHandler(async (req, res) =>{
+      const { chatId, chatName } = req.body;
+      const updatedChat = await Chat.findByIdAndUpdate(
+        chatId,
+        {
+          chatName: chatName,
+        },
+        {
+          new: true,
+        }
+      )
+      .populate("users", "-password")
+        .populate("groupAdmin", "-password");
+        if (!updatedChat) {
+          res.status(404);
+          throw new Error("Chat Not Found");
+        } else {
+          res.json(updatedChat);
+        }
+      });
+
+
 });
 module.exports = {accessChat,
-fetchChats,createGroupChat};
+fetchChats,createGroupChat,renameGroup};
