08733292f8ed4b153967df00b4ea49672d359c11
Rudecapybere <affithusky@gmail.com>
2023-04-08 18:12:50 +0530

    group chat api

--- Backend/routes/chatRoutes.js
+++ Backend/routes/chatRoutes.js
@@ -3,13 +3,13 @@ const { protect } = require("../middleware/authMiddleware");
 
 const {
   accessChat,
-  fetchChats
+  fetchChats,createGroupChat
 } = require("../controllers/chatControllers");
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
-// router.route("/group").post(protect, createGroupChat);
+router.route("/group").post(protect, createGroupChat);
 // router.route("/rename").put(protect, renameGroup);
 // router.route("/groupremove").put(protect, removeFromGroup);
 // router.route("/groupadd").put(protect, addToGroup);
