22d67fb7d429af5dedfbb15672465ceef2685391
Shiv13527 <yuvrajpundir7788@gmail.com>
2023-04-11 13:18:39 +0530

    created chatpovider context

---
+++ Frontend/src/Context/ChatProvider.js
@@ -0,0 +1,9 @@
+import { createContext } from 'react'
+
+const ChatContext = createContext();
+
+const ChatProvider = ({children}) => {
+    return <ChatContext.Provider>{children}</ChatContext.Provider>;
+};
+
+export default ChatProvider;
