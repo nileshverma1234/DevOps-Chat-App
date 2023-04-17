14606240ae6425e679ff1ebfed9698a6db2f3ea1
Shiv13527 <yuvrajpundir7788@gmail.com>
2023-04-17 19:32:44 +0530

    Completed UpdatedGroupChatModal, Rm user fn

--- Frontend/src/Components/SingleChat.js
+++ Frontend/src/Components/SingleChat.js
@@ -43,6 +43,21 @@ const SingleChat = ({fetchAgain, setFetchAgain}) => {
                     </>
                 )}
                 </Text>
+
+                <Box  
+                    display="flex"
+                    flexDir="column"
+                    justifyContent="flex-end"
+                    p={3}
+                    bg="#E8E8E8"
+                    w="100%"
+                    h="100%"
+                    borderRadius="lg"
+                    overflowY="hidden"
+                
+                >
+                    {/* {Messages Here} */}
+                </Box>
             </>
         ) : (
             <Box display="flex" alignItems="center" justifyContent="center" h="100%">
