1dc826c8a4ac71db4e64a9aa37d5be00937cd6d9
Shiv13527 <yuvrajpundir7788@gmail.com>
2023-04-17 19:11:41 +0530

    Created ChatBox UI, updated ChatLogics.js

--- Frontend/src/Components/SingleChat.js
+++ Frontend/src/Components/SingleChat.js
@@ -1,6 +1,10 @@
 import { Box, Text} from "@chakra-ui/layout"
-import React from 'react'
-import { ChatState } from "../Context/ChatProvider"
+import { IconButton } from '@chakra-ui/react'
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import React from 'react';
+import { ChatState } from "../Context/ChatProvider";
+import { getSender, getSenderFull } from "../config/ChatLogics";
+import ProfileModal from "./miscellaneous/ProfileModal";
 
 const SingleChat = ({fetchAgain, setFetchAgain}) => {
 
@@ -8,7 +12,38 @@ const SingleChat = ({fetchAgain, setFetchAgain}) => {
    return (
       <>
         {selectedChat ? (
-            <></>
+            <>
+                <Text
+                    fontSize={{ base: "28px", md: "30px" }}
+                    pb={3}
+                    px={2}
+                    w="100%"
+                    fontFamily="Work sans"
+                    display="flex"
+                    justifyContent={{ base: "space-between" }}
+                    alignItems="center"
+                >
+                <IconButton
+                    display={{ base: "flex", md: "none" }}
+                    icon={<ArrowBackIcon />}
+                    onClick={() => setSelectedChat("")}
+                />
+                {!selectedChat.isGroupChat ? (
+                    <>
+                        {getSender(user, selectedChat.users)}
+                        <ProfileModal user={getSenderFull(user, selectedChat.users)}/>
+                    </>
+                ) : (
+                    <>{selectedChat.chatName.toUpperCase()}
+                        {/* <UpdateGroupChatModal
+                            fetchAgain={fetchAgain}
+                            setFetchAgain={setFetchAgain} 
+                            /> */}
+                  
+                    </>
+                )}
+                </Text>
+            </>
         ) : (
             <Box display="flex" alignItems="center" justifyContent="center" h="100%">
                 <Text fontSize="3x1" pb={3} fontFamily="Work Sans">
