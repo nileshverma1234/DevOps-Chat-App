d338b6ad25721dcead395eda067f72d2c5eda28b
Yuvraj7788 <85951644+Yuvraj7788@users.noreply.github.com>
2023-04-28 00:59:12 +0530

    sendMessage UI, logic and fetch all msgs

--- Frontend/src/Components/SingleChat.js
+++ Frontend/src/Components/SingleChat.js
@@ -1,100 +1,183 @@
-import { Box, Text} from "@chakra-ui/layout"
-import { IconButton, Spinner, useToast } from '@chakra-ui/react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Text } from "@chakra-ui/layout";
+import { IconButton, Spinner, useToast, Input, effect } from "@chakra-ui/react";
 import { ArrowBackIcon } from "@chakra-ui/icons";
-import React from 'react';
+import axios from "axios";
 import { ChatState } from "../Context/ChatProvider";
 import { getSender, getSenderFull } from "../config/ChatLogics";
 import ProfileModal from "./miscellaneous/ProfileModal";
+import UpdateGroupChatModal from "./miscellaneous/UpdatedGroupChatModal";
+
 import { FormControl } from "@chakra-ui/form-control";
+import "./styles.css";
+const SingleChat = ({ fetchAgain, setFetchAgain }) => {
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [newMessage, setNewMessage] = useState("");
+  const [socketConnected, setSocketConnected] = useState(false);
+  const [typing, setTyping] = useState(false);
+  const [istyping, setIsTyping] = useState(false);
+  const toast = useToast();
 
-const SingleChat = ({fetchAgain, setFetchAgain}) => {
+  const fetchMessages = async () => {
+    if (!selectedChat) return;
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer" + user.token,
+        },
+      };
+      setLoading(true);
+      const { data } = await axios.get(
+        "/api/message/" + selectedChat._id,
+        config
+      );
 
-    const [messages, setMessages] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [newMessage, setNewMessage] = useState("");
-    const [socketConnected, setSocketConnected] = useState(false);
-    const [typing, setTyping] = useState(false);
-    const [istyping, setIsTyping] = useState(false);
-    const toast = useToast(); 
+      setMessages(data);
+      setLoading(false);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: "Failed to Load the Messages",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+    }
+  };
 
-   const { user, selectedChat, setSelectedChat} = ChatState(); 
-   return (
-      <>
-        {selectedChat ? (
-            <>
-                <Text
-                    fontSize={{ base: "28px", md: "30px" }}
-                    pb={3}
-                    px={2}
-                    w="100%"
-                    fontFamily="Work sans"
-                    display="flex"
-                    justifyContent={{ base: "space-between" }}
-                    alignItems="center"
-                >
-                <IconButton
-                    display={{ base: "flex", md: "none" }}
-                    icon={<ArrowBackIcon />}
-                    onClick={() => setSelectedChat("")}
+  useEffect(() => {
+    fetchMessages();
+  }, [selectedChat]);
+
+  const sendMessage = async (event) => {
+    if (event.key == "Enter" && newMessage) {
+      try {
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer ${user.token}",
+          },
+        };
+
+        const { data } = await axios.post(
+          "/api/message",
+          {
+            content: newMessage,
+            chatId: selectedChat._id,
+          },
+          config
+        );
+
+        console.log(data);
+
+        setNewMessage("");
+        setMessages([...messages, data]);
+      } catch (error) {
+        toast({
+          title: "Error Occured!",
+          description: "Failed to send the Message",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom",
+        });
+      }
+    }
+  };
+
+  const typingHandler = (e) => {
+    setNewMessage(e.target.value);
+    //Typing Indicator logic
+  };
+  const { user, selectedChat, setSelectedChat } = ChatState();
+  return (
+    <>
+      {selectedChat ? (
+        <>
+          <Text
+            fontSize={{ base: "28px", md: "30px" }}
+            pb={3}
+            px={2}
+            w="100%"
+            fontFamily="Work sans"
+            display="flex"
+            justifyContent={{ base: "space-between" }}
+            alignItems="center"
+          >
+            <IconButton
+              display={{ base: "flex", md: "none" }}
+              icon={<ArrowBackIcon />}
+              onClick={() => setSelectedChat("")}
+            />
+            {!selectedChat.isGroupChat ? (
+              <>
+                {getSender(user, selectedChat.users)}
+                <ProfileModal user={getSenderFull(user, selectedChat.users)} />
+              </>
+            ) : (
+              <>
+                {selectedChat.chatName.toUpperCase()}
+                <UpdateGroupChatModal
+                  fetchAgain={fetchAgain}
+                  setFetchAgain={setFetchAgain}
+                  fetchMessages={fetchMessages}
                 />
-                {!selectedChat.isGroupChat ? (
-                    <>
-                        {getSender(user, selectedChat.users)}
-                        <ProfileModal user={getSenderFull(user, selectedChat.users)}/>
-                    </>
-                ) : (
-                    <>{selectedChat.chatName.toUpperCase()}
-                        {/* <UpdateGroupChatModal
-                            fetchAgain={fetchAgain}
-                            setFetchAgain={setFetchAgain} 
-                            /> */}
-                  
-                    </>
-                )}
-                </Text>
+              </>
+            )}
+          </Text>
 
-                <Box  
-                    display="flex"
-                    flexDir="column"
-                    justifyContent="flex-end"
-                    p={3}
-                    bg="#E8E8E8"
-                    w="100%"
-                    h="100%"
-                    borderRadius="lg"
-                    overflowY="hidden"
-                
-                >
-                    {loading ? (
-                        <Spinner
-                        size="xl"
-                        w={20}
-                        h={20}
-                        alignSelf="center"
-                        margin="auto"
-                        />
-                    ):(
-                        <div>
-                            {/* message */}
-                        </div>
-                    )}
+          <Box
+            display="flex"
+            flexDir="column"
+            justifyContent="flex-end"
+            p={3}
+            bg="#E8E8E8"
+            w="100%"
+            h="100%"
+            borderRadius="lg"
+            overflowY="hidden"
+          >
+            {loading ? (
+              <Spinner
+                size="xl"
+                w={20}
+                h={20}
+                alignSelf="center"
+                margin="auto"
+              />
+            ) : (
+              <div className="messages">{/* message */}</div>
+            )}
 
-                    <FormControl>
-                        
-                    </FormControl>
-                </Box>
-            </>
-        ) : (
-            <Box display="flex" alignItems="center" justifyContent="center" h="100%">
-                <Text fontSize="3x1" pb={3} fontFamily="Work Sans">
-                    Click on a useer to start chatting
-                </Text>
-
-            </Box>
-
-        )}
-      </>
-   );
-}
+            <FormControl onKeyDown={sendMessage} isRequired mt={3}>
+              <Input
+                variant="filled"
+                bg="#E0E0E0"
+                placeholder="Enter a message.."
+                value={newMessage}
+                onChange={typingHandler}
+              />
+            </FormControl>
+          </Box>
+        </>
+      ) : (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          h="100%"
+        >
+          <Text fontSize="3x1" pb={3} fontFamily="Work Sans">
+            Click on a useer to start chatting
+          </Text>
+        </Box>
+      )}
+    </>
+  );
+};
 
 export default SingleChat;
