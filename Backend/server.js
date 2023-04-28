798bf9a1e7536928a8068b6a5828240f48bcb826
Yuvraj7788 <85951644+Yuvraj7788@users.noreply.github.com>
2023-04-28 22:31:42 +0530

    socket io setup, join chat and real time msgs

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const { Socket } = require("socket.io");
 //const bodyParser = require('body-parser');
 dotenv.config();
 
@@ -29,4 +30,40 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, console.log(`server is started on port ${PORT}`));
+const server = app.listen(
+  5000,
+  console.log(`server is started on port ${PORT}`)
+);
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", () => {
+  console.log("connected to socket.io");
+
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("user joined room: " + room);
+  });
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat users not defined");
+
+    chat.users.forEach((user) => {
+      if ((user._id == newMessageRecieved.sender, _id)) return;
+
+      socket.in(user._id).emit("message received", newMessageRecieved);
+    });
+  });
+});
